c34a763e912710e7a3d8fd4dea5266a6c7badd76
Elior <elior@gmail.com>
2026-04-13 15:42:33 +0300

    Initial commit

---
+++ src/test/java/com/example/employees_demo/EmployeesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employees_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
